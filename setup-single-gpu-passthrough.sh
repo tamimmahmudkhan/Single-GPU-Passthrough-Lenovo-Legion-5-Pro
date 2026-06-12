50583e2a748b17124def0b5e5acf1bfb3dba3637
tamimmahmudkhan <khan.mahmud96@yahoo.com>
2026-06-12 17:04:55 +0900

    Fix script edits.

--- setup-single-gpu-passthrough.sh
+++ setup-single-gpu-passthrough.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
- # sudo mkdir -p /etc/libvirt/hooks/qemu.d/win11/{prepare/begin,release/end}
+ sudo mkdir -p /etc/libvirt/hooks/qemu.d/win11/{prepare/begin,release/end}
 
- # sudo cp qemu /etc/libvirt/hooks/
- # sudo chmod +x /etc/libvirt/hooks/qemu
+ sudo cp qemu /etc/libvirt/hooks/
+ sudo chmod +x /etc/libvirt/hooks/qemu
 
  sudo cp start.sh /etc/libvirt/hooks/qemu.d/win11/prepare/begin/
  sudo cp revert.sh /etc/libvirt/hooks/qemu.d/win11/release/end/
